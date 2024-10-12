88004f2c877ce492f4025578269ad2305910c6d1
Tiris <100187552+EgorChernishevskiy@users.noreply.github.com>
2024-10-12 18:50:24 +0400

    Auth endpoint has benn added

---
+++ Back/WordleBack/src/score/router.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from src import database
+from src.auth import schemas, models
+
+router = APIRouter()
+
+# Топ 10 пользователей по score
+@router.get("/top", response_model=list[schemas.UserOut])
+async def get_top_players(db: AsyncSession = Depends(database.get_db)):
+    async with db as session:
+        result = await session.execute(select(models.User).order_by(models.User.score.desc()).limit(10))
+        top_players = result.scalars().all()
+        return top_players
+
+# Получение score конкретного пользователя
+@router.get("/user/{user_name}/score", response_model=schemas.UserScore)
+async def get_user_score(user_name: str, db: AsyncSession = Depends(database.get_db)):
+    async with db as session:
+        result = await session.execute(select(models.User).filter(models.User.user_name == user_name))
+        user = result.scalars().first()
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
